a109d9cd42fe945329cab2e3e1c0cb26d9b2e2f2
AlishaMomin <am05757@st.habib.edu.pk>
2022-07-13 21:08:20 +0500

    backened folders

--- CRM/server/index.js
+++ CRM/server/index.js
@@ -1,11 +1,20 @@
 // server/index.js
 
 const express = require("express");
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-
+// ------------------------------------------------------------
+app.use(cors());
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// ------------------------------------------------------------
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
