6747c2379a8480cdfaf807b26d99b7f2a837bc57
AlishaMomin <am05757@st.habib.edu.pk>
2022-07-13 22:30:44 +0500

    api documentation

--- CRM/server/index.js
+++ CRM/server/index.js
@@ -1,24 +1,32 @@
-// server/index.js
-
 const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
 const swaggerJsDoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
 
-const PORT = process.env.PORT || 3001;
+
+const adapter = new FileSync("db.json");
+const db = low(adapter);
+
+
+db.defaults({ books: [] }).write();
+const booksRouter = require("./routes/books");
+const optionOpenAPI = require("./openapi.json")
 
 const app = express();
-// ------------------------------------------------------------
-app.use(cors());
-// Configuring body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-// ------------------------------------------------------------
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
-});
+app.db = db;
+const port = process.env.PORT || 5000;
+const swaggerDocs = swaggerJsDoc(optionOpenAPI);
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use("/books", booksRouter);
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.listen(port, () => {
+  console.log(`server listening on port ${port}`);
 });
