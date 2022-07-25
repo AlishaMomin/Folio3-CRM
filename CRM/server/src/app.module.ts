7f907848c594cea3dd6b8281a8aaea7d3f358cec
AlishaMomin <am05757@st.habib.edu.pk>
2022-07-25 22:51:49 +0500

    order, orderline, user (entity, controller, service, module)

--- CRM/server/src/app.module.ts
+++ CRM/server/src/app.module.ts
@@ -2,13 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
+// role
 import { role } from './role/entity/role.entity';
 import { RoleModule } from './role/role.module';
+// company
 import { company } from './company/entity/company.entity';
 import { CompanyModule } from './company/company.module';
-import { ProductModule } from './product/product.module';
+// product
 import { product } from './product/entity/product.entity';
+import { ProductModule } from './product/product.module';
+// order
+import { order } from './order/entity/order.entity';
+import { OrderModule } from './order/order.module';
+// orderline
+import { orderline } from './orderline/entity/orderline.entity';
+import { OrderlineModule } from './orderline/orderline.module';
+// user
+import { user } from './user/entity/user.entity';
+import { UserModule } from './user/user.module';
+
+
 
 
 @Module({
@@ -19,9 +32,9 @@ import { product } from './product/entity/product.entity';
     username: 'root',
     password: 'Yaaliallah786!',
     database: 'crm',
-    entities: [role, company, product],
+    entities: [role, company, product, order,orderline,user],
     synchronize: true,
-  }), CompanyModule, RoleModule, ProductModule
+  }), CompanyModule, RoleModule, ProductModule, OrderModule, OrderlineModule, UserModule
   ],
   controllers: [AppController],
   providers: [AppService],
